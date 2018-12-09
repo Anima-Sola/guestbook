0ae583cdeb1a7ec8bb54e29199d6729b2dc0c94e
Ivan Mikhaylenko <animasola@yandex.ru>
2018-12-09 18:01:41 +0300

    Поработал над модальными окнами

--- guestbook/modules/ModalWindow/js/ModalWindow.js
+++ guestbook/modules/ModalWindow/js/ModalWindow.js
@@ -32,12 +32,12 @@
         var top = scrollTop + clientHeight/2 - heightInPx/2;
         var left = clientWidth/2 - widthInPx/2;
 
-        styles = "top: " + top + "px; " +
-                 "left: " + left  + "px; " +
-                 "width: " +  widthInPx + "px; " +
-                 "height: " + heightInPx + "px; " +
-                 "max-width: " + clientWidth + "px; " +
-                 "max-height: " + clientHeight + "px;";
+        styles = `top: ${top}px;
+                  left: ${left}px;
+                  width: ${widthInPx}px;
+                  height: ${heightInPx}px;
+                  max-width: ${clientWidth}px;
+                  max-height: ${clientHeight}px;`;
 
         return styles;
         
@@ -73,37 +73,24 @@
         var top = scrollTop + clientHeight/2 - heightInPx/2 - 12;
         var left = clientWidth/2 + widthInPx/2 - 12;
 
-        styles = "top: " + top + "px; " +
-                 "left: " + left  + "px; ";
+        styles = `top: ${top}px;
+                  left: ${left}px;`;
 
         return styles;
-        
-    }
-    
-    // Получаем код события onclick закрытия модального окна
-    function getCloseWindowEvent() {
-        
-        var events;
-        
-        events = "modalWindow.closeModalWindow(\"modalWindow\");";
-        
-        return events;        
         
     }
 
     // Закрытие модального окна
     function closeModalWindow() {
         
-        var modalWindow = document.getElementById('modalWindow');
-        modalWindow.remove();
+        document.getElementById('modalWindow').remove();
         
     }
     
     //Открытие модального окна
-    //id - элемента div модального окна в DOM
     //width - ширина модального окна
     //height - высота модального окна
-    //measureType - единица измерения для width и height (px, em, % и т.д.)
+    //measureType - единица измерения для width и height (px, % и т.д.)
     //content - содержимое окна (html)
     function showModalWindow(width, height, measureType, content) {
 
@@ -115,21 +102,20 @@
         body.insertBefore(modalWindowDiv, body.firstChild);
         
         var modalWindow = document.getElementById('modalWindow');
- 
-        html = "<div class='modal-window__overlay' onclick='" + getCloseWindowEvent() + "'></div>" +
-                   "<div class='modal-window__content' style='" + getCenterWindowStyles(width, height, measureType) + "'>" +
-                        "<div class='modal-window__content-wrapper'>" + 
-                            "<div>" + content + "</div>" +
-                        "</div>" +
-                    "</div>" +
-               "<div class='modal-window__cross' onclick='" + getCloseWindowEvent() + "' style='" + getCrossStyles(width, height, measureType) + "'>X</div>";
-        modalWindow.innerHTML = html;
-        
+
+        html = `<div class="modal-window__overlay" onclick="modalWindow.closeModalWindow();"></div>
+                    <div class="modal-window__content" style="${getCenterWindowStyles(width, height, measureType)}">
+                        <div class="modal-window__content-wrapper">
+                            <div>${content}</div>
+                        </div>
+                    </div>
+                <div class="modal-window__cross" onclick="modalWindow.closeModalWindow();" style="${getCrossStyles(width, height, measureType)}">X</div>`;
+
+        modalWindow.innerHTML = html;        
     }
 
     modalWindow.showModalWindow = showModalWindow;
     modalWindow.closeModalWindow = closeModalWindow;
-    
     window.modalWindow = modalWindow;
 
 })();
